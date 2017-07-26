d00f61e35f25046b69944f6471d5b520449b6ae4
Hassaan Zaidi <Hassaan Zaidi>
2017-07-26 22:54:12 +0200

    :sparkles: Removed deprecated mongo functions. Added location  support for map component

--- server/data/index.js
+++ server/data/index.js
@@ -4,11 +4,17 @@ const config = require('../../project.config')
 function connectData () {
   if (config.db) {
     if (config.db.type === 'mongo') {
-      return mongoose.connect('mongodb://localhost/test')
+      mongoose.Promise = global.Promise
+      return mongoose.createConnection('mongodb://localhost/test', {
+        poolSize: 4,
+        useMongoClient: true,
+      })
     } else if (config.db.type === 'local') {
       // @TODO creae a wrapper for db so different databases can be swapped
+      return Promise.reject(new Error(`Database option "${config.db.type}" not yet supported`))
     } else if (config.db.type === 'postgres') {
-
+      // @TODO support switching database to postgres
+      return Promise.reject(new Error(`Database option "${config.db.type}" not yet supported`))
     }
   } else {
     return Promise.reject(new Error('No Database configuration found in project.config'))
--- src/routes/Map/components/MapView.js
+++ src/routes/Map/components/MapView.js
@@ -3,28 +3,52 @@ import PropTypes from 'prop-types'
 import GoogleMapReact from 'google-map-react'
 import './MapView.scss'
 
-export const MapView = ({
-  center,
-  zoom,
-  setMapDefault,
-  setMapZoom,
-  findStationsPoint,
-  findStationsPostCode
-}) => (
-  <div>
-    <div className='mapView col-md-10'>
-      <GoogleMapReact
-        defaultCenter={center}
-        defaultZoom={zoom}
-      >
-        <span>{center.lng} - {center.lat}</span>
-      </GoogleMapReact>
-    </div>
-    <div className='mapResults col-md-2'>
-      asd
-    </div>
-  </div>
-)
+class MapView extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  componentDidMount () {
+    const self = this
+    if ('geolocation' in navigator) {
+      /* geolocation is available */
+      navigator.geolocation.getCurrentPosition((position) => {
+        const lat = position.coords.latitude
+        const lng = position.coords.longitude
+        if (self.props.center.lng !== lng && self.props.center.lat !== lat) {
+          console.log('Setting center to:', lng, lat)
+          self.props.setMapDefault({ lat, lng })
+        }
+      })
+    }
+  }
+
+  render () {
+    const {
+      center,
+      zoom,
+      setMapDefault,
+      setMapZoom,
+      findStationsPoint,
+      findStationsPostCode
+    } = this.props
+    return (
+      <div>
+        <div className='mapView col-md-10'>
+          <GoogleMapReact
+            defaultCenter={center}
+            defaultZoom={zoom}
+          >
+            <span>{center.lng} - {center.lat}</span>
+          </GoogleMapReact>
+        </div>
+        <div className='mapResults col-md-2'>
+          asd
+        </div>
+      </div>
+    )
+  }
+}
 
 MapView.propTypes = {
   center: PropTypes.object.isRequired,
@@ -36,3 +60,4 @@ MapView.propTypes = {
 }
 
 export default MapView
+export { MapView }
